93b435ed9b4a708346ed62449e34aa04e8025ef7
thapar <pulkitthapar25@gmail.com>
2021-05-03 19:10:12 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+import 'Screens/WelcomeScreen/AcceptTerms.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Auth',
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: AcceptTerms(),
+    );
+  }
+}
